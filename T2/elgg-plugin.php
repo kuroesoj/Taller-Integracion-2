5c275eac2b958b291bb2409159da40f52b5e6a8f
Jalderete <74800565+kuroesoj@users.noreply.github.com>
2021-10-27 11:33:34 -0300

    Add files via upload

---
+++ T2/elgg-plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'routes' => [
+                'default:T2' => [
+                        'path' => '/T2',
+                        'resource' => 'T2',
+                ],
+        ],
+];
